179d561ddc5484301d691ebdc4d87d1762d398e5
Taylor McKenzie <xx20twinkiie14xx@gmail.com>
2019-04-04 23:36:46 -0700

    Began and Completed Assignment
    All Functions Created

--- Assignment 09/Using References/Source.cpp
+++ Assignment 09/Using References/Source.cpp
@@ -3,14 +3,49 @@
 
 using namespace std;
 
+void transaction(int x, int y);
+void hackTrans(int& x, int& y);
+
+
 int main()
 {
-		//Initialized variable
-	int myScore = 100;
-		//Nickname reference
-	int& royScore = myScore;
+	cout << "Welcome to the 'Checks and Bounces' Client" << endl;
+	//Current balances
+	int myBalance;
+	int cpuBalance;
+	cout << "Please enter your balance." << endl;
+	cin >> myBalance;
+	cout << "Please enter the CPU's balance." << endl;
+	cin >> cpuBalance;
 
+	cout << "\nOriginal Balances" << endl;
+	cout << "User: " << myBalance << endl;
+	cout << "CPU: " << cpuBalance << endl;
 
-	cout << "The variable myScore has a value of " << royScore << "." << endl;
-	system("pause");
+	cout << "\nRegular Transaction" << endl;
+	transaction(myBalance, cpuBalance);
+	cout << "User: " << myBalance << endl;
+	cout << "CPU: " << cpuBalance << endl;
+
+	cout << "\nHacked Transaction" << endl;
+	hackTrans(myBalance, cpuBalance);
+	cout << "User: " << myBalance << endl;
+	cout << "CPU: " << cpuBalance << endl;
+
+	cout << "\n";
+	system("Pause");
+}
+
+void transaction(int x, int y)
+{
+	int tempVar = x;
+	x = y;
+	y = tempVar;
+}
+
+void hackTrans(int& x, int& y)
+{
+	int tempVar = x;
+	x = y;
+	y = tempVar;
 }
